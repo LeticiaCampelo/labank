7f2e81fe46de44ae6ff497f424d63749a29928ad
SpaceLizzy <leticia.campelo@hotmail.com>
2020-09-24 15:09:39 -0300

    Setup do projeto com SpringBoot

---
+++ src/main/java/com/labank/LabankApplication.java
@@ -0,0 +1,13 @@
+package com.labank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labank/LabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

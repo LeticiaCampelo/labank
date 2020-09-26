4a60d11c5246cf670d70d7aad64c7d7eef444599
SpaceLizzy <leticia.campelo@hotmail.com>
2020-09-25 23:18:08 -0300

    Ajuste na generic controller

--- src/main/java/com/controller/GenericController.java
+++ src/main/java/com/controller/GenericController.java
@@ -2,10 +2,12 @@ package com.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class GenericController {
 
 	private static final int SUCCESS = 200;

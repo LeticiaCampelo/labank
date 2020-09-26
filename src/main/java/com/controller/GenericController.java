bd5e93eaea1281380831b2451ac31a81b28a3df7
SpaceLizzy <leticia.campelo@hotmail.com>
2020-09-26 19:11:37 -0300

    rotas no index

--- src/main/java/com/controller/GenericController.java
+++ src/main/java/com/controller/GenericController.java
@@ -19,15 +19,28 @@ public class GenericController {
 	ResponseEntity<String> getAllBearers() {
 		log.info("GET /");
 		String routes = "Bearer routes:\n"
-						+ "GET and PUT https://labank.herokuapp.com/api/v1/bearer/{id} "
-						+ "\nPOST https://labank.herokuapp.com/api/v1/bearer\n\n"
+						+ "GET and PUT https://labank.herokuapp.com/api/v1/bearer/{id} valid ids: 39744197056 or 69995668000112"
+						+ "\nPOST https://labank.herokuapp.com/api/v1/bearer\nValid Json: \n {\r\n" + 
+																								"    \"bearerDocument\": \"69995668000112\",\r\n" + 
+																								"    \"bearerName\": \"Leticia\",\r\n" + 
+																								"    \"bearerType\": 0\r\n" + 
+																								"}\n\n"
 					    + "Account routes:"
-					    + "\nGET https://labank.herokuapp.com/api/v1/account/{id},"
-					    + "\nPOST https://labank.herokuapp.com/api/v1/account\n\n"
+					    + "\nGET https://labank.herokuapp.com/api/v1/account/{id}  valid accounts: 02451027 or 031818652"
+					    + "\nPOST https://labank.herokuapp.com/api/v1/account\nValid Json: \n {\r\n" + 
+																							    "        \"accountNumber\": \"031818652\",\r\n" + 
+																							    "        \"accountBalance\": 4500.0,\r\n" + 
+																							    "        \"bearer\": \"699956680112\",\r\n" + 
+																							    "        \"agency\": 2\r\n" + 
+																							    "}\n\n"
 					    + "Transactions routes: "
-					    + "\nGET https://labank.herokuapp.com/api/v1/transaction/{id}, "
+					    + "\nGET https://labank.herokuapp.com/api/v1/transaction/{id}, Valid accounts: 02451027 or 031818652"
 					    + "\nPOST https://labank.herokuapp.com/api/v1/transaction/deposit"
-					    + "\n     https://labank.herokuapp.com/api/v1/transaction/withdraw";
+					    + "\n     https://labank.herokuapp.com/api/v1/transaction/withdraw\n"
+					    + "Valid Json: \n{\r\n" + 
+					    "            \"transactionAmount\": 5000,\r\n" + 
+					    "            \"accountNumber\": \"031818652\"\r\n" + 
+					    			"}";
 		return ResponseEntity.status(SUCCESS).body(routes);
 	}
 }
